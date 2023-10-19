cf3d6a31489e90f78cb8c9b5a4d856c9d883fbbb
Mike Smithson <mikesmithso@gmail.com>
2023-10-19 13:27:49 -0400

    use lombok to remove boilerplate constructor in InvoiceController

--- src/main/java/com/oreilly/pos/controller/InvoiceController.java
+++ src/main/java/com/oreilly/pos/controller/InvoiceController.java
@@ -1,6 +1,7 @@
 package com.oreilly.pos.controller;
 
 import com.oreilly.pos.service.InvoiceService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,13 +12,10 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/invoices")
+@AllArgsConstructor
 public class InvoiceController {
-    private InvoiceService invoiceService;
 
-    @Autowired
-    public InvoiceController(InvoiceService invoiceService) {
-        this.invoiceService = invoiceService;
-    }
+    private InvoiceService invoiceService;
 
     @GetMapping("/customer/{customerId}")
     public Map<Long, String> retrieveInvoice(@PathVariable long customerId) {
