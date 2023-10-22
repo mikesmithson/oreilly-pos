94fd8bac5ef6c94c3c5a459b779bd5ebbf78e605
Mike Smithson <mikesmithso@gmail.com>
2023-10-22 12:26:48 -0400

    add @Repository stereotype annotaion to repository class

--- src/main/java/com/oreilly/pos/repository/InvoiceRepository.java
+++ src/main/java/com/oreilly/pos/repository/InvoiceRepository.java
@@ -2,9 +2,10 @@ package com.oreilly.pos.repository;
 
 import com.oreilly.pos.entity.InvoiceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface InvoiceRepository extends JpaRepository<InvoiceEntity, Long> {
      List<InvoiceEntity> findInvoiceEntitiesByCustomerId(long customerId);
 }
